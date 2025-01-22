6ae66b582a88c406d37730898b949a1eec71c7a5
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2025-01-22 23:44:32 +0200

    Created base structure before inserting Inna code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VueRouter({
+      /* options */
+    }),
+    vue(),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+      dts: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    }),
+  ],
 })
